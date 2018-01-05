2b47eedee2e464fa78a3f8a3c5bae8892f03b3f2
mayu <trachan15-wan4@nifty.com>
2018-01-05 14:15:21 +0900

    ball  C#     左右に動きません

---
+++ Assets/Ball.cs
@@ -0,0 +1,40 @@
+﻿
+
+using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour
+{
+
+    //変数の定義　（データを入れる箱を作る）
+
+    public float moveSpeed;
+    private Rigidbody rb;
+
+
+
+    void Start()
+    {
+        //代入　（箱の中に右辺のデータを入れる）
+
+        rb = GetComponent<Rigidbody>();
+
+    }
+
+
+    // Update is called once per frame
+
+    void Update()
+    {
+
+        float moveH = Input.GetAxis("Horizontal");
+      Debug.Log(moveH);
+        float moveV = Input.GetAxis("Vertical");
+      Debug.Log(moveV);
+        Vector3 movement = new Vector3(moveH, 0, moveV);
+        rb.AddForce(movement * moveSpeed);
+
+    }
+}
+
+
