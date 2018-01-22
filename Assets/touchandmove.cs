fa1b1ea62224ea5144cdb679734d37b8bed4cc12
mayu <trachan15-wan4@nifty.com>
2018-01-22 18:20:38 +0900

    上に動かすこと、　りんご　を　かごへ移動すること　勉強中です

--- Assets/touchandmove.cs
+++ Assets/touchandmove.cs
@@ -2,51 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class touchandmove : MonoBehaviour {
+public class NewBehaviourScript : MonoBehaviour {
 
-
-
-    Object = apple200
-
-
-
-    // Use this for initialization
-    void Start () {
-
-                         
-                       }
-
-
-                    
-
-    // Update is called once per frame
-     void Update () {
-
-
-
-    if (Input.GetMouseButtonDown(0))
-    {
-        Ray ray = new Ray();
-        RaycastHit hit = new RaycastHit();
-        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-
-        //マウスクリックした場所からRayを飛ばし、オブジェクトがあればtrue 
-        if (Physics.Raycast(ray.origin, ray.direction, out hit, Mathf.Infinity))
-        {
-            if (hit.collider.gameObject == this.gameObject)
-            {
-                //ここにりんごをクリックした時の処理
-
-                RaycastHit hit;
-                Ray ray = camera.ScreenPointToRay(Input.mousePosition);
-
-                if (Physics.Raycast(ray, out hit))
-                {
-                    Transform objectHit = hit.transform;
-
-                    // レイに当たったオブジェクトに何かをする
-
-
-
-                }
-            }
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
