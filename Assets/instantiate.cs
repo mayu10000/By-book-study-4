71c654be040965c91e9773c7799dced4860dfce3
mayu <trachan15-wan4@nifty.com>
2018-01-30 19:36:06 +0900

    くす玉　をみてください

---
+++ Assets/instantiate.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class instantiate : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+        var cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- Assets/starprint.cs
+++ Assets/starprint.cs
@@ -1,1 +1,27 @@
-﻿
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class starprint : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+
+        string str;
+        str = " " ;
+
+for (int i = 0; i < 4; i++)
+        {
+            str += "*";
+            Debug.Log(str);
+        }
+
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
