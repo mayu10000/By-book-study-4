b68b3e12c27ec9428e4f9924d003301504e0695d
mayu <trachan15-wan4@nifty.com>
2018-02-05 20:24:45 +0900

    紙吹雪を大きくしたいのです　グラス内にボールをたくさんつくりたいです　水を入れているようにみせる

--- Assets/instantiate.cs
+++ Assets/instantiate.cs
@@ -2,18 +2,50 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class instantiate : MonoBehaviour {
+public class instantiate : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
+    // GmaeObjectを生成する数
+    public int createNum = 200;
 
-        var cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+    // Use this for initialization
+    void Start()
+    {
 
+        for (int i = 0; i < createNum; i++)
+        {
+            // ゲームオブジェクトを生成
+            var obj = GameObject.CreatePrimitive(PrimitiveType.Cube);
+
+            // 接触判定を無効に
+            obj.GetComponent<BoxCollider>().enabled = false;
+
+            // R, G, Bをそれぞれランダムな値に設定する
+            float rrand = Random.Range(0, 1.0f);
+            float grand = Random.Range(0, 1.0f);
+            float brand = Random.Range(0, 1.0f);
+            obj.GetComponent<Renderer>().material.color = new Color(rrand, grand, brand, 1);
+
+            // Rigidbodyをアタッチ
+            Rigidbody rig = obj.AddComponent<Rigidbody>();
+
+            // 親オブジェクトを設定
+            obj.transform.SetParent(transform);
+
+            // Local positionを設定
+            obj.transform.localPosition = Vector3.zero;
+
+            // 生成したGameObjectにランダムな力を加える
+            float xrand = Random.Range(-5.0f, 5.0f);
+            float yrand = Random.Range(-20.0f, 0);
+            float zrand = Random.Range(-5.0f, 5.0f);
+            rig.velocity = new Vector3(xrand, yrand, zrand);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
 
     }
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 }
