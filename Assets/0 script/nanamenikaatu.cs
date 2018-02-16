e832240cb4d4ee7e4ad89cb327ded6b9d0274f14
mayu <trachan15-wan4@nifty.com>
2018-02-16 21:21:22 +0900

    雪だるま（に見えませんが）と　うさぎ　　加圧して滑るように見せたいのです  　script    nanamenikaatu

---
+++ Assets/0 script/nanamenikaatu.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class nanamenikaatu : MonoBehaviour {
+
+  //  public class Test : MonoBehaviour
+  //  {
+
+        Rigidbody myRigidbody;
+
+        //前進するための力（追加）
+        private float forwardForce = 800.0f;
+
+        // Use this for initialization
+        void Start()
+        {
+            myRigidbody = GetComponent<Rigidbody>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            this.myRigidbody.AddForce(new Vector3(0, -1.0f, 1.0f) * this.forwardForce);
+        }
+    }
+
+
+
+    // Use this for initialization
+   // void Start () {
+		
+//	}
+	
+	// Update is called once per frame
+ //	void Update () {
+		
+//	}
+// }
