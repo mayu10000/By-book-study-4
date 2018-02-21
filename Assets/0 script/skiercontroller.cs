4d522522be79374fc3831f4682b2f5e0be0b3c38
mayu <trachan15-wan4@nifty.com>
2018-02-21 20:20:29 +0900

    少しレイアウトを進めました　動きはまだまだです

---
+++ Assets/0 script/skiercontroller.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class skiercontroller : MonoBehaviour {
+
+      Rigidbody2D rigid2D;
+   // Rigidbody3D rigid3D;
+
+
+    Animator animator;
+
+
+    
+
+    float jumpForce = 680.0f;
+    float runForce = 30.0f;
+    float maxRunSpeed = 2.0f;
+    float threshold = 0.2f;
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        // 左右移動
+        int key = 0;
+        if (Input.GetKey(KeyCode.RightArrow)) key = 1;
+        if (Input.GetKey(KeyCode.LeftArrow)) key = -1;
+
+
+        if (Input.acceleration.x > this.threshold) key = 1;
+        if (Input.acceleration.x < -this.threshold) key = -1;
+
+        // プレーヤーの速度
+        float speedx = Mathf.Abs(this.rigid2D.velocity.x);
+
+       
+
+
+
+        // スピード制限
+    //    if (speedx < this.maxWalkSpeed)
+    //    {
+    //        this.rigid2D.AddForce(transform.right * key * this.walkForce);
+    //    }
+
+
+
+        // スピード制限
+        if (speedx < this.maxRunSpeed)
+        {
+            this.rigid2D.AddForce(transform.right * key * this.runForce);
+        }
+
+
+        
+        // 動く方向に応じて反転
+        if (key != 0)
+        {
+            transform.localScale = new Vector3(key, 1, 1);
+        }
+
+
+        
+    }
+}
