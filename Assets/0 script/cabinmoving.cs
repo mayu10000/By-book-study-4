cbf527d068d483061d4723cfdcb062fcc864948b
mayu <trachan15-wan4@nifty.com>
2018-03-04 00:50:05 +0900

    原因はわかりませんが　また動かなくなりました　

---
+++ Assets/0 script/cabinmoving.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+// public GameObject applePrefab;
+//public GameObject cabin ;
+
+
+
+
+public class cabinmoving : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+
+        // int a ;
+        // int b ;
+        // int c ;
+
+
+        Vector3 a = new Vector3(315, 26, 273) ;
+        Vector3 b = new Vector3(280, 54, 243) ;
+        Vector3 c = b - a;
+
+   
+
+         //   item.transform.position = new Vector3(x, 4, z);
+          //  cabin.transform.position = Vector3 c;
+
+        //     cabin.transform.position =  c;
+
+
+        // cabin.transform.position = new Vector3(x, y, z);
+        //   cabin.positon = cabin.transform.position
+
+
+
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/0 script/nanamenikaatu3cabin.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class nanamenikaatu3cabin  : MonoBehaviour {
+    
+
+    Rigidbody myRigidbody;
+
+    //前進するための力（追加）
+    private float forwardForce = 200.0f;
+
+
+    // Use this for initialization
+    void Start () {
+
+        myRigidbody = GetComponent<Rigidbody>();
+
+            }
+	
+	// Update is called once per frame
+	void Update () {
+
+
+        //   this.myRigidbody.AddForce(new Vector3(0, -1.0f,  1.0f) * this.forwardForce);
+
+        float xVec = 0;
+        float yVec = -1.0f;
+
+        // 左矢印を押している時にx軸に正の方向に力を加える
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            // こちらの値は動きに合わせて調整してください
+            xVec = 2;
+        }
+
+        // 左矢印を押している時にx軸に正の方向に力を加える
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            // こちらの値は動きに合わせて調整してください
+            xVec = -2;
+        }
+
+        // スペースを押した時にy軸に正の方向に力を加える
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // こちらの値は動きに合わせて調整してください  *****
+            //  yVec = 5;
+            yVec = 100;
+
+        }
+
+
+        //   this.myRigidbody.AddForce(new Vector3(0, -1.0f,  1.0f) * this.forwardForce);
+
+      //  this.myRigidbody.AddForce(new Vector3(xVec, yVec, -1.0f) * this.forwardForce);
+
+        this.myRigidbody.AddForce(new Vector3(xVec, yVec, 0.5f) * this.forwardForce);
+    }
+}
+
+
+	
+
