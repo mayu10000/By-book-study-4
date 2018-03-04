b944b4fb2a0e86eae4d0dff6d16e42db0ddbb15f
mayu <trachan15-wan4@nifty.com>
2018-03-04 22:44:36 +0900

    cabin  の　Line　での移動

---
+++ Assets/0 script/ToGerandeScenedirector.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.SceneManagement; // LoadScene を使うために必要！！
+
+
+
+
+public class ToGerandeScenedirector : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //クリックされたら・・・・scene を遷移する
+        //  ***** ここを　UI　ボタンに変更する
+        if (Input.GetMouseButtonDown(0))
+
+
+
+        {
+           // SceneManager.LoadScene("GameScene");
+            SceneManager.LoadScene("gerande 1");
+
+        }
+    }
+}
+
+
+	
+
---
+++ Assets/1 sample from suzuki san/DelayedSample/Delayed.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Delayed : MonoBehaviour
+{
+    public float m_delayTime = 8f;
+    public GameObject m_gameObject;
+    float m_timer;
+
+    void Start()
+    {
+        m_gameObject.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (!m_gameObject.activeSelf)
+        {
+            if (m_timer < m_delayTime)
+            {
+                m_timer += Time.deltaTime;
+            }
+            else
+            {
+                m_gameObject.SetActive(true);
+            }
+        }
+    }
+}
